2f9c43262a5c0038294bd18227462ffbdc78f6b3
hayatemraan <emraanhasanhayat@gmail.com>
2023-07-29 20:17:01 +0600

    shopping-cart

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import LogIn from "./pages/LogIn";
+import Verify from "./pages/Verify";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/otp" element={<Verify />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
